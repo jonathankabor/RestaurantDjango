a3133ac68c8f728de9cadaa31bd2b53defa51ca8
unknown <kaborejbaime@gmail.com>
2026-05-24 18:51:21 +0200

    Lire les objets de la base de données

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Burger
 
 
 # / menu
 def index(request):
-    return HttpResponse("Bienvenue sur le Menu du restaurant")
+    Burgers = Burger.objects.all()
+    Burgers_list = ','.join([str(burger) for burger in Burgers])
+    return HttpResponse(f"Bienvenue sur le Menu du restaurant. Burgers disponibles : {Burgers_list}")
 # Create your views here.
