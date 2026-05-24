995b9f491e5da0c4b922b2d074e5f3f4004d219c
unknown <kaborejbaime@gmail.com>
2026-05-24 19:13:28 +0200

    Rajouter le prix des burgers

--- menu/views.py
+++ menu/views.py
@@ -6,6 +6,6 @@ from .models import Burger
 # / menu
 def index(request):
     Burgers = Burger.objects.all()
-    Burgers_list = ','.join([str(burger) for burger in Burgers])
+    Burgers_list = ','.join([str(burger) + " : " + str(burger.prix) + "€" for burger in Burgers])
     return HttpResponse(f"Bienvenue sur le Menu du restaurant. Burgers disponibles : {Burgers_list}")
 # Create your views here.
